3ffe6557f1beda26f760a9e8f08c7b73206fb35d
marcrijksen <marcrijksen@gmail.com>
2018-06-15 17:11:35 +0200

    Spinner für benachrichtigungen eingestellt

--- app/src/main/java/com/example/nathalieseibert/wells/Benachrichtigungseinstellungen.java
+++ app/src/main/java/com/example/nathalieseibert/wells/Benachrichtigungseinstellungen.java
@@ -1,12 +1,17 @@
 package com.example.nathalieseibert.wells;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 /**
@@ -14,6 +19,9 @@ import android.support.v4.app.Fragment;
  */
 public class Benachrichtigungseinstellungen extends Fragment {
 
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     public Benachrichtigungseinstellungen() {
         // Required empty public constructor
@@ -23,8 +31,27 @@ public class Benachrichtigungseinstellungen extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_benachrichtigungseinstellungen, container, false);
+        //Spinner die Werte zuteilen
+
+        View view = inflater.inflate(R.layout.fragment_benachrichtigungseinstellungen, container, false);
+        spinner = view.findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.frequenz_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), parent.getItemAtPosition(position) + " ausgewählt", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return view;
     }
 
 }
