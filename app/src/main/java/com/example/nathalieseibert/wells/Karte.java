d01aa20bb3295a56cbb31ef642eb8e82a604f5a0
marcrijksen <marcrijksen@gmail.com>
2018-07-09 16:15:42 +0200

    Maps mit marker geht!

--- app/src/main/java/com/example/nathalieseibert/wells/Karte.java
+++ app/src/main/java/com/example/nathalieseibert/wells/Karte.java
@@ -4,9 +4,6 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -28,6 +25,7 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Karte extends Fragment implements OnMapReadyCallback {
@@ -40,8 +38,9 @@ public class Karte extends Fragment implements OnMapReadyCallback {
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private Boolean permissionsGranted = false;
+    Integer clickCount;
     int nummer;
-    Float [] longitude = {
+    Float[] longitude = {
             47.115089f, 47.045327f, 47.10901f, 47.05199f, 47.096147f, 47.071981f, 47.048412f, 47.07825f, 47.066614f, 47.075883f,
             47.072459f, 47.066627f, 47.09328f, 47.070637f, 47.063031f, 47.06186f, 47.02739f, 47.076479f, 47.06895f, 47.059141f, 47.066898f,
             47.062052f, 47.076082f, 47.053402f, 47.050887f, 47.083247f, 47.071227f, 47.071055f, 47.070949f, 47.067908f, 47.082082f, 47.083574f,
@@ -50,21 +49,21 @@ public class Karte extends Fragment implements OnMapReadyCallback {
             47.125381f, 47.066532f, 47.104295f, 47.094226f, 47.072855f, 47.032149f, 47.06873f, 47.070815f, 47.073891f, 47.080962f, 47.054426f, 47.061674f,
             47.075087f, 47.064635f, 47.070939f, 47.076406f, 47.044583f, 47.076365f, 47.072823f, 47.073364f, 47.054417f, 47.101121f};
 
-    Float [] latitude = {15.42118f,15.44534f,15.412398f,15.438784f,15.411977f,15.422085f,15.47099f,15.422457f,15.446348f,15.458124f,15.395426f,15.453491f,
-            15.411475f,15.435787f,15.436526f,15.436911f,15.45207f,15.395841f,15.434699f,15.434386f,15.431779f,15.453553f,15.450246f,15.463884f,15.456462f,15.433708f,
-            15.438135f,15.438419f,15.438617f,15.397388f,15.46014f,15.46047f,15.402819f,15.44164f,15.442456f,15.422729f,15.446924f,15.426019f,15.435743f,15.440225f,
-            15.445772f,15.445371f,15.453286f,15.430721f,15.435614f,15.44205f,15.451863f,15.451177f,15.424811f,15.427498f,15.443722f,15.468962f,15.417544f,15.416739f,
-            15.441898f,15.442234f,15.439245f,15.420142f,15.474925f,15.436251f,15.396315f,15.459086f,15.456367f,15.437379f,15.431217f,15.47439f,15.472214f,15.445712f,
-            15.451561f,15.433647f,15.458355f,15.443198f,15.404832f,15.427923f,15.427451f,15.403278f,15.411737f};
+    Float[] latitude = {15.42118f, 15.44534f, 15.412398f, 15.438784f, 15.411977f, 15.422085f, 15.47099f, 15.422457f, 15.446348f, 15.458124f, 15.395426f, 15.453491f,
+            15.411475f, 15.435787f, 15.436526f, 15.436911f, 15.45207f, 15.395841f, 15.434699f, 15.434386f, 15.431779f, 15.453553f, 15.450246f, 15.463884f, 15.456462f, 15.433708f,
+            15.438135f, 15.438419f, 15.438617f, 15.397388f, 15.46014f, 15.46047f, 15.402819f, 15.44164f, 15.442456f, 15.422729f, 15.446924f, 15.426019f, 15.435743f, 15.440225f,
+            15.445772f, 15.445371f, 15.453286f, 15.430721f, 15.435614f, 15.44205f, 15.451863f, 15.451177f, 15.424811f, 15.427498f, 15.443722f, 15.468962f, 15.417544f, 15.416739f,
+            15.441898f, 15.442234f, 15.439245f, 15.420142f, 15.474925f, 15.436251f, 15.396315f, 15.459086f, 15.456367f, 15.437379f, 15.431217f, 15.47439f, 15.472214f, 15.445712f,
+            15.451561f, 15.433647f, 15.458355f, 15.443198f, 15.404832f, 15.427923f, 15.427451f, 15.403278f, 15.411737f};
 
-    String [] name = {"Am Eichengrund","Andersengasse","Andritzer Reichsstraße","Angergasse","Augasse","Babenbergerstraße","Banngrabenweg","Darmstadtgasse","Dietrichsteinplatz",
-            "Dürergasse","Eggenberger Allee","Felix-Dahn Platz","Fischeraustraße","Franziskanerplatz","Friedrichgasse 1","Friedrichgasse 2","Gasometerweg","Georgigasse","Grieskai 1",
-            "Grieskai 2","Griesplatz","Hafnerriegel","Halbärthgasse","Harmsdorfgasse 1","Harmsdorfgasse 2","Hasnerplatz","Hauptplatz 1","Hauptplatz 2","Hauptplatz 3","Hauseggerstraße",
-            "Hilmteichstraße 1","Hilmteichstraße 2","Hofbauerplatz","Hofgasse","Jakominiplatz","Josef-Huber Gasse","Kaiser-Josef-Platz","Kantgasse","Kapistran-Pieller-Platz",
-            "Karmeliterplatz","Kasernstraße","Lagergasse","Lendplatz","Marburger Kai","Maria-Theresia-Allee","Max-Mell- Allee","Naglergasse","Nippelgasse","Oeverseegasse","Ortweinplatz",
-            "Petrifelderstraße","Pongratz-Moore-Steg 1","Pongratz-Moore-Steg 2","Puchsteg","Radegunder Straße","Radetzkystraße","Rohrbachergasse","Roseggerweg","Sackstraße","Salfeldstraße",
-            "Schillerplatz","Schillerstraße","Schlossberg","Schwimmschulkai","St. Peter Hauptstraße","St.-Peter-Pfarrweg","Stadtpark","Stremayrgasse","Südtiroler Platz","Tegetthoffplatz",
-            "Theyergasse","Vinzenzgasse","Volksgartenstraße 1","Volksgartenstraße 2","Wachtelgasse","Wasserwerkgasse"};
+    String[] name = {"Am Eichengrund", "Andersengasse", "Andritzer Reichsstraße", "Angergasse", "Augasse", "Babenbergerstraße", "Banngrabenweg", "Darmstadtgasse", "Dietrichsteinplatz",
+            "Dürergasse", "Eggenberger Allee", "Felix-Dahn Platz", "Fischeraustraße", "Franziskanerplatz", "Friedrichgasse 1", "Friedrichgasse 2", "Gasometerweg", "Georgigasse", "Grieskai 1",
+            "Grieskai 2", "Griesplatz", "Hafnerriegel", "Halbärthgasse", "Harmsdorfgasse 1", "Harmsdorfgasse 2", "Hasnerplatz", "Hauptplatz 1", "Hauptplatz 2", "Hauptplatz 3", "Hauseggerstraße",
+            "Hilmteichstraße 1", "Hilmteichstraße 2", "Hofbauerplatz", "Hofgasse", "Jakominiplatz", "Josef-Huber Gasse", "Kaiser-Josef-Platz", "Kantgasse", "Kapistran-Pieller-Platz",
+            "Karmeliterplatz", "Kasernstraße", "Lagergasse", "Leechgasse", "Lendplatz", "Marburger Kai", "Maria-Theresia-Allee", "Max-Mell- Allee", "Naglergasse", "Nippelgasse", "Oeverseegasse", "Ortweinplatz",
+            "Petrifelderstraße", "Pongratz-Moore-Steg 1", "Pongratz-Moore-Steg 2", "Puchsteg", "Radegunder Straße", "Radetzkystraße", "Rohrbachergasse", "Roseggerweg", "Sackstraße", "Salfeldstraße",
+            "Schillerplatz", "Schillerstraße", "Schlossberg", "Schwimmschulkai", "St. Peter Hauptstraße", "St.-Peter-Pfarrweg", "Stadtpark", "Stremayrgasse", "Südtiroler Platz", "Tegetthoffplatz",
+            "Theyergasse", "Vinzenzgasse", "Volksgartenstraße 1", "Volksgartenstraße 2", "Wachtelgasse", "Wasserwerkgasse"};
 
     public Karte() {
     }
@@ -180,7 +179,7 @@ public class Karte extends Fragment implements OnMapReadyCallback {
                         .build();                   // Creates a CameraPosition from the builder
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-            }else{
+            } else {
                 CameraPosition Graz = CameraPosition.builder().target(GRAZ).zoom(13).build();
                 googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Graz));
 
@@ -193,13 +192,45 @@ public class Karte extends Fragment implements OnMapReadyCallback {
         }
 
 
-        for (int i = 1; i < 76; i++) {
+        for (int i = 0; i < 77; i++) {
             googleMap.addMarker(new MarkerOptions()
                     .position(new LatLng(longitude[i], latitude[i]))
-                    .title(name [i]));
+                    .title(name[i])
+
+            );
+
         }
 
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                // Retrieve the data from the marker.
+
+
+
+                /* DB statistik
+
+                clickCount = (Integer) marker.getTag();
+
+                // Check if a click count was set, then display the click count.
+                    clickCount = clickCount + 1;
+                    marker.setTag(clickCount);
+                    Toast.makeText(getActivity(),
+                            marker.getTitle() +
+                                    " has been clicked " + clickCount + " times.",
+                            Toast.LENGTH_SHORT).show();
+                */
+
+                // Return false to indicate that we have not consumed the event and that we wish
+                // for the default behavior to occur (which is for the camera to move such that the
+                // marker is centered and for the marker's info window to open, if it has one).
+
+                return false;
+            }
+        });
     }
+
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -213,22 +244,6 @@ public class Karte extends Fragment implements OnMapReadyCallback {
         }
 
     }
-
-
-    private final SensorEventListener mSensorListener = new SensorEventListener() {
-
-        @Override
-        public void onSensorChanged(SensorEvent arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor arg0, int arg1) {
-            // TODO Auto-generated method stub
-
-        }
-    };
 
 
 }
