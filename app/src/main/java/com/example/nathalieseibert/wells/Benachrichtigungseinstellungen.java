bac9f86805484c1b9a12497a4b413f082745061b
marcrijksen <marcrijksen@gmail.com>
2018-07-11 11:27:12 +0200

    layout benachrichtigungen steht!

--- app/src/main/java/com/example/nathalieseibert/wells/Benachrichtigungseinstellungen.java
+++ app/src/main/java/com/example/nathalieseibert/wells/Benachrichtigungseinstellungen.java
@@ -1,8 +1,7 @@
 package com.example.nathalieseibert.wells;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -11,19 +10,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
-import static android.content.Context.ALARM_SERVICE;
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Benachrichtigungseinstellungen extends Fragment {
-
+    Spinner spinner;
+    Switch switchben;
+    EditText datum;
+    DatePickerDialog.OnDateSetListener date;
+    Calendar datumpicker;
+    EditText zeitpicker;
+    Spinner wspinner;
+    ArrayAdapter<CharSequence> adapter;
+    ArrayAdapter<CharSequence> adapterw;
 
     public Benachrichtigungseinstellungen() {
         // Required empty public constructor
@@ -35,59 +46,164 @@ public class Benachrichtigungseinstellungen extends Fragment {
 
         // Inflate the layout for this fragment
         //Spinner die Werte zuteilen
-        Spinner spinner;
-        ArrayAdapter<CharSequence> adapter;
+
         View view = inflater.inflate(R.layout.fragment_benachrichtigungseinstellungen, container, false);
         spinner = view.findViewById(R.id.spinner);
+        switchben = view.findViewById(R.id.switchonben);
+        zeitpicker = view.findViewById(R.id.time_view_edit);
+        wspinner = view.findViewById(R.id.spinnerw);
+
+        adapterw = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.wochentage, android.R.layout.simple_spinner_item);
+        adapterw.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.frequenz_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        wspinner.setAdapter(adapterw);
+        datumpicker = Calendar.getInstance();
+        datum = view.findViewById(R.id.datum);
+        datum.setVisibility(View.GONE);
+        zeitpicker.setVisibility(View.GONE);
+        wspinner.setVisibility(View.GONE);
 
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                datumpicker.set(Calendar.YEAR, year);
+                datumpicker.set(Calendar.MONTH, monthOfYear);
+                datumpicker.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        datum.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(getActivity(), date, datumpicker
+                        .get(Calendar.YEAR), datumpicker.get(Calendar.MONTH),
+                        datumpicker.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        zeitpicker.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+
+                        zeitpicker.setText(selectedHour + ":" + selectedMinute);
+                        // TODO ALARM SETZEN
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity().getBaseContext(), parent.getItemAtPosition(position) + " ausgewählt", Toast.LENGTH_SHORT).show();
 
+                switch (position) {
+                    case 0:
+                        datum.setVisibility(View.GONE);
+                        wspinner.setVisibility(View.GONE);
+                        zeitpicker.setVisibility(View.GONE);
+                        break;
+                    case 1:
+                        datum.setVisibility(View.GONE);
+                        wspinner.setVisibility(View.GONE);
+                        zeitpicker.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        datum.setVisibility(View.GONE);
+                        wspinner.setVisibility(View.VISIBLE);
+                        zeitpicker.setVisibility(View.VISIBLE);
+                        break;
+                    case 3:
+                        wspinner.setVisibility(View.GONE);
+                        datum.setVisibility(View.VISIBLE);
+                        zeitpicker.setVisibility(View.VISIBLE);
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getActivity().getBaseContext(), "Nichts ausgewählt", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        wspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getActivity().getBaseContext(), parent.getItemAtPosition(position) + " ausgewählt", Toast.LENGTH_SHORT).show();
 
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
                 Toast.makeText(getActivity().getBaseContext(), "Nichts ausgewählt", Toast.LENGTH_SHORT).show();
             }
         });
-
-
+        /*
         view.findViewById(R.id.speichernButon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Das nächste sollte statt den addWater button auf den Benachrichtigung speichern Button bei den Einstellungen geändert werden!
                 //Folgend wird täglich um 8:30 eine Notification erscheinen
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY, 7);
-                calendar.set(Calendar.MINUTE, 10);
-                calendar.set(Calendar.SECOND, 0);
 
-                Intent intent = new Intent(getActivity().getApplicationContext(), MyNotificationPublisher.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity().getApplicationContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
+                if (switchben.isChecked()) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(Calendar.HOUR_OF_DAY, 7);
+                    calendar.set(Calendar.MINUTE, 10);
+                    calendar.set(Calendar.SECOND, 0);
 
-                try {
-                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-                } catch (NullPointerException ignored) {
-
+                    Intent intent = new Intent(getActivity().getApplicationContext(), MyNotificationPublisher.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity().getApplicationContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
+                    Toast.makeText(getActivity(), "Alarm wurde gesetzt!", Toast.LENGTH_SHORT).show();
+                    try {
+                        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+                    } catch (NullPointerException ignored) {
+                        Toast.makeText(getActivity(), "Kann Alarm nicht setzen!", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
 
 
         });
+        */
         return view;
+    }
 
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.GERMAN);
 
+        datum.setText(sdf.format(datumpicker.getTime()));
+        // TODO ALARM SETZEN
     }
 
 
